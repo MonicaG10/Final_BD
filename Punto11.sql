c4d9ec37b50d81d39fac9ede83e351e3a407df3b
Monica Maria Gutierrez Ortiz <monicagutierrez116099@correo.itm.edu.co>
2018-11-26 21:22:14 -0500

    primera parte querys

---
+++ Punto11.sql
@@ -0,0 +1,62 @@
+--PUNTO 1
+
+CREATE TYPE number_array AS VARRAY(100) OF INTEGER;
+
+--Se crea la función
+CREATE OR REPLACE FUNCTION divisible_sum_pairs(Enteros IN number_array, K IN INTEGER, OUT  ) 
+RETURN INTEGER AS 
+resultado number;
+BEGIN
+    IF (1<=K<=100) THEN 
+    --
+    ELSE
+    END IF;
+return resultado;
+commit;
+end;
+/
+DECLARE
+k  number := 0;
+Enteros ARRAY := (1,2,3,4,5);
+BEGIN
+divisible_sum_pairs(k,Enteros);
+END;
+
+/
+
+--PUNTO 3
+create or replace Function find_digits  (n in integer, d in integer , resultado out integer) 
+return integer as
+    n integer;
+    aux integer;
+    residuo integer := 0;
+    Divisor integer;
+    resultado integer;
+begin
+    n := n;
+    aux := n;
+    d := d;
+    residuo:=0;
+    while (aux>0)
+    loop
+        Divisor:= mod(aux,d);
+        residuo := residuo + Divisor;
+        aux:=trunc(aux/d);
+    end loop;
+    
+    if n = 0 then
+         resultado := residuo;
+    else
+         dbms_output.put_line('El numero ingresado no es divisor de' || d);    
+ end if; 
+return resultado;
+commit;
+end;
+/
+declare
+n number := 12;
+d number := 3;
+begin
+find_digits(n,d);
+dbms_output.put_line('resultado: ' || resultado);
+end;
